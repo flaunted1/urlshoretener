8feae727a4f03010f946abceaa8e36a30f66f65a
flaunted1 <againflaunted@gmail.com>
2026-05-20 16:05:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { nanoid } = require('nanoid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const links = new Map();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/api/shorten', (req, res) => {
+  const { url } = req.body;
+
+  if (!url || !url.startsWith('http')) {
+    return res.status(400).json({
+      error: 'Please enter a valid URL'
+    });
+  }
+
+  const id = nanoid(6);
+
+  links.set(id, url);
+
+  res.json({
+    shortUrl: `${req.protocol}://${req.get('host')}/${id}`
+  });
+});
+
+app.get('/:id', (req, res) => {
+  const destination = links.get(req.params.id);
+
+  if (!destination) {
+    return res.status(404).send('Link not found');
+  }
+
+  res.redirect(destination);
+});
+
+app.listen(PORT, () => {
+  console.log(`Hazy running on port ${PORT}`);
+});
